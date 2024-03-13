53a1d72b81528aac80944b739489039137b51a72
Claire <claire.github-309c@sitedethib.com>
2024-03-13 15:05:53 +0100

    Add tests

--- spec/services/after_block_domain_from_account_service_spec.rb
+++ spec/services/after_block_domain_from_account_service_spec.rb
@@ -9,12 +9,15 @@ RSpec.describe AfterBlockDomainFromAccountService do
   let!(:alice) { Fabricate(:account, username: 'alice') }
 
   before do
+    NotificationPermission.create!(account: alice, from_account: wolf)
+
     wolf.follow!(alice)
   end
 
-  it 'purge followers from blocked domain and sends `Reject->Follow` accordingly' do
+  it 'purge followers from blocked domain, remove notification permissions, and sends `Reject->Follow` accordingly' do
     expect { subject.call(alice, 'evil.org') }
       .to change { wolf.following?(alice) }.from(true).to(false)
+      .and change { NotificationPermission.exists?(account: alice, from_account: wolf) }.from(true).to(false)
 
     expect(ActivityPub::DeliveryWorker).to have_enqueued_sidekiq_job(/Reject/, alice.id, wolf.inbox_url)
   end
